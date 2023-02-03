4fb2e402d6eae4e487a9bb2a9ab481add1a76b7e
deano4 <dnp2k4@gmail.com>
2023-02-02 21:46:58 -0800

    upto min 15 of the vid
    upto min 15 of the video. Made board with 5 rows

---
+++ Da Gamee/Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Match-3/Item")]
+public sealed class Item : MonoBehaviour
+{
+    public int value; //points
+    
+    public Sprite sprite;
+
+}
